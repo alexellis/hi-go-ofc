0898f98cfc552fb5e1e271b248a810aafad14488
Alex Ellis <alexellis2@gmail.com>
2019-11-08 18:07:32 +0000

    Update handler.go

--- run-on-lambda/handler.go
+++ run-on-lambda/handler.go
@@ -6,5 +6,5 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello from Lambda: %s", string(req))
+	return fmt.Sprintf("Hallo von der Lambda: %s", string(req))
 }
