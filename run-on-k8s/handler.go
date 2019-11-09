4e46b59fc2234925fb2baf07cabedeebd090d544
Alex Ellis <alexellis2@gmail.com>
2019-11-09 11:21:48 +0000

    Update handler.go

--- run-on-k8s/handler.go
+++ run-on-k8s/handler.go
@@ -6,5 +6,5 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("e2e tls on K8s, great eh: %s", string(req))
+	return fmt.Sprintf("I'm running on K8s and have full e2e TLS from you to my Pod %s", string(req))
 }
